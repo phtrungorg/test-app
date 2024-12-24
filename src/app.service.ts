d79af12c2d46a41a8d9abcb5bcd0187fa57eb75f
phtrung <phtrungit@gmail.com>
2024-12-24 10:49:33 +0700

    syntax-test

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getGreeting(name: string): string {
+    return `Hello, ${name; // Missing closing curly brace and parenthesis
+  }
 }
