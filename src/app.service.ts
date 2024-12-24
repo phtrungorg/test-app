e30c13ef46096d40aadb9cb23539bd3d7ea57d2c
phtrung <phtrungit@gmail.com>
2024-12-24 11:09:09 +0700

    clean-test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,20 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World!";
+  }
+
+  calculate(array: number[]): { max: number; sum: number } {
+    let max = -Infinity;
+    let sum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i]; // Find the maximum
+      }
+      sum += array[i]; // Calculate the sum
+    }
+
+    return { max, sum };
   }
 }
