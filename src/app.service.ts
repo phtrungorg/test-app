966c538e947865d9fb6a249ed89894e5b665369f
phtrung <phtrungit@gmail.com>
2024-12-24 17:32:59 +0700

    main

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,18 +7,4 @@ export class AppService {
   }
   // create the function with logical error infinite loop
   // the function is used to retry the request if the request is failed
-
-  calculateAverage(numbers: number[]): number {
-    if (!numbers || numbers.length === 0) {
-      return 0; // Return 0 if the array is empty
-    }
-
-    let total = 0;
-
-    for (let i = 0; i < numbers.length - 1; i++) {
-      total += numbers[i];
-    }
-
-    return total / numbers.length;
-  }
 }
